025d756bee20c27db328ee0d4e9203850fcac7e4
prog4life <prog4life@users.noreply.github.com>
2017-12-28 18:50:47 +0200

    App bar, rename to style.css

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,50 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavBar from './NavBar';
 
-class AppBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isNavBarOpened: false
-    };
-  }
-  render() {
-    return (
-      <header className="app-bar">
-        <button
-          className="icon-button app-bar__open-sidenav"
-          onClick={() => {
-            this.setState({ isNavBarOpened: true });
-          }}
-          type="button"
-        >
-          {'MENU'}
-        </button>
-        <h1>{'Local Wall'}</h1>
-        <NavBar
-          isOpened={this.state.isNavBarOpened}
-          onClose={() => (
-            this.setState({ isNavBarOpened: false })
-          )}
-        />
-        <div className="app-bar__toolbar">
-          <button
-            className="icon-button app-bar__auth-button"
-            type="button"
-          >
-            {'SIGN IN'}
-          </button>
-          <button
-            className="icon-button app-bar__options-button"
-            type="button"
-          >
-            {'OPTIONS'}
-          </button>
-        </div>
-      </header>
-    );
-  }
-}
+const AppBar = props => (
+  <header className="app-bar">
+    <h1>Local Wall</h1>
+    <NavBar />
+  </header>
+);
 
 export default AppBar;
