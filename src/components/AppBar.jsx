1750e93040004005eb99d8aaf677d9f56df3a820
prog4life <prog4life@users.noreply.github.com>
2017-12-29 02:08:38 +0200

    Side navbar in progress

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,6 +3,7 @@ import NavBar from './NavBar';
 
 const AppBar = props => (
   <header className="app-bar">
+    <div className="menu-button">{'MENU'}</div>
     <h1>Local Wall</h1>
     <NavBar />
   </header>
