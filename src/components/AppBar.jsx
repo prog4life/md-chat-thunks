23cafe78dbfd04029ad394df28f9aa87a11df68d
prog4life <prog4life@users.noreply.github.com>
2017-12-30 04:01:34 +0200

    Enable side nav sliding toggle, add more styling

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBar from './NavBar';
 
-const AppBar = props => (
-  <header className="app-bar">
-    <div className="menu-button">{'MENU'}</div>
-    <h1>Local Wall</h1>
-    <NavBar />
-  </header>
-);
+class AppBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isNavBarOpened: false
+    };
+  }
+  render() {
+    return (
+      <header className="app-bar">
+        <button
+          className="sidenav-button app-bar__open-sidenav"
+          onClick={() => {
+            this.setState({ isNavBarOpened: true });
+          }}
+          type="button"
+        >
+          {'MENU'}
+        </button>
+        <h1>{'Local Wall'}</h1>
+        <NavBar
+          isOpened={this.state.isNavBarOpened}
+          onClose={() => (
+            this.setState({ isNavBarOpened: false })
+          )}
+        />
+      </header>
+    );
+  };
+};
 
 export default AppBar;
