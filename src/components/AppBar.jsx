0711a6c0b6a09ecdc34b7ef1852929275a6d6779
prog4life <prog4life@users.noreply.github.com>
2018-05-09 16:25:24 +0300

    Create basic AppBar (NavBar) component

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
+// import PropTypes from 'prop-types';
+import {
+  Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarToggler, Collapse,
+} from 'reactstrap';
 
 import AppLogo from './AppLogo';
 
 class AppBar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+  handleToggling = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen,
+    }));
+  }
   render() {
+    const { isOpen } = this.state;
+
     return (
-      // <Navbar dark color="">
-      <Navbar>
+      // <Navbar color="light" fixed="top">
+      <Navbar light style={{ backgroundColor: '#fdecece3' }} expand="md">
         <NavbarBrand href="/">
           <AppLogo />
         </NavbarBrand>
+        <NavbarToggler onClick={this.handleToggling} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" pills navbar>
+            <NavItem>
+              <NavLink href="/chats">
+                {'Chats'}
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/profile">
+                {'Profile'}
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/settings">
+                {'Settings'}
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
     );
   }
